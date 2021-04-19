243a02c97f7995872a8d724c4965449d4ff880e3
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-19 03:36:33 +0200

    Update btn component

--- components/button/Button.js
+++ components/button/Button.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Link from 'next/link';
 import classes from './button.module.css';
-const Button = ({ link, children }) => {
+const Button = ({ link, children, onClick }) => {
+  if (link) {
+    return (
+      <Link href={link}>
+        <a className={classes.btn}>{children}</a>
+      </Link>
+    );
+  }
+
   return (
-    <Link href={link}>
-      <a className={classes.btn}>{children}</a>
-    </Link>
+    <button className={classes.btn} onClick={onClick}>
+      {children}
+    </button>
   );
 };
 
