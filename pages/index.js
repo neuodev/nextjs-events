65c9aec029344fb3bcbd7af35d78bb79fd6766e4
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-19 02:04:08 +0200

    Add styles for the event-item

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import EventList from '../components/events/EventList';
 import { getFeaturedEvents } from '../dummy-data';
 
 const HomePage = () => {
   const featuresEvents = getFeaturedEvents();
-  
+
   return (
     <div>
-      <h1>Home Page</h1>
+      <EventList events={featuresEvents} />
     </div>
   );
 };
