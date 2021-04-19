f97c8b16a4ebb644937b82c5dfe933d8fcd3d8ce
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-19 02:53:01 +0200

    Add Event Details page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,6 +1,9 @@
 import React from 'react';
-
+import { useRouter } from 'next/router';
+import { getEventById } from '../../dummy-data';
 const EventPage = () => {
+  const router = useRouter();
+  const event = getEventById(router.query.eventId);
   return (
     <div>
       <h1>Envet Page</h1>
