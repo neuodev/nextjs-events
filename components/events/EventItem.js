5fff1ab28119a933049e45491515d20be2c314cc
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-19 02:29:52 +0200

    Add icons to the EventItem

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import Link from 'next/link';
 import Button from '../button/Button';
 import classes from './event-item.module.css';
+import DateIcon from '../icons/date-icon';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
 
 const EventItem = ({ event }) => {
   const { title, image, date, location, id } = event;
@@ -21,15 +23,22 @@ const EventItem = ({ event }) => {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
         </div>
         <div className={classes.address}>
+          <AddressIcon />
           <address>{formattedAddress}</address>
         </div>
-      </div>
-      <div className={classes.actions}>
-        <Button link={exploreLink}>Explore Event</Button>
+        <div className={classes.actions}>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
+        </div>
       </div>
     </li>
   );
