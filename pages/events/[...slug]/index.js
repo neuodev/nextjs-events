7de46cb8437c0fe3cf56f9d9adc4fffedd90d0c0
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-19 04:04:02 +0200

    Parse the input to neumric value

--- pages/events/[...slug]/index.js
+++ pages/events/[...slug]/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import { getFilteredEvents } from '../../../dummy-data';
+import { useRouter } from 'next/router';
+import EventList from '../../../components/events/EventList';
 const FilterEvents = () => {
+  const router = useRouter();
+  const query = router.query.slug;
+  if (!query) return <p>Filters are required</p>;
+  console.log(query);
+  const events = getFilteredEvents({
+    year: parseInt(query[0]),
+    month: parseInt(query[1]),
+  });
+  if (events.length == 0) return <h1>No Evets Found</h1>;
   return (
     <div>
-      <h1>Filter Events</h1>
+      <EventList events={events} />
     </div>
   );
 };
